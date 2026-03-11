11a65a247757cbedf9d0054c5c661aa6715fd048
Ivan Shanygin <shanygin101@yandex.ru>
2026-03-11 04:08:41 +0300

    Fixed null pointer dereference

--- nulldump/nulldump.c
+++ nulldump/nulldump.c
@@ -56,8 +56,10 @@ static ssize_t nulldump_write(struct file *file, const char __user *data,
 }
 
 static char *nulldump_devnode(const struct device *dev, umode_t *mode) {
-  *mode = S_IRUGO | S_IWUGO; // rw-rw-rw-
-  return NULL;
+  if (mode != NULL) {
+    *mode = S_IRUGO | S_IWUGO; // rw-rw-rw-
+  }
+  return 0;
 }
 
 static struct file_operations nulldump_fops = {
